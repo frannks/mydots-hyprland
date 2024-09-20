7e9456d50346dfd9523d42d8b3eccff0182470ca
Franklin Souza <fraank@riseup.net>
2024-09-19 22:59:34 -0300

    Commit automático no repositório mydots-hyprland!

--- .config/waybar/scripts/powermenu.sh
+++ .config/waybar/scripts/powermenu.sh
@@ -12,8 +12,12 @@ log_out="󰍃 Log Out"
 # Options passed to wofi
 options="$power_off\n$reboot\n$lock_screen\n$suspend\n$hibernate\n$log_out"
 
+# Define posições para centralizar
+x=820
+y=280
+
 # Exibe todas as opções de uma vez usando wofi
-chosen=$(echo -e "$options" | wofi --dmenu --insensitive --prompt "Power Menu:" --width 280 --height 290)
+chosen=$(echo -e "$options" | wofi --dmenu --insensitive --prompt "Power Menu:" --width 280 --height 290 --x $x --y $y)
 
 case $chosen in
     "$lock_screen")
